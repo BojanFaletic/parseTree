25ff355ad27c0d556d9fe2c89f18074ef943c681
Bojan Faletic <bfaletic@tae.com>
2022-11-17 08:56:59 +0100

    formatting

--- main.c
+++ main.c
@@ -5,37 +5,31 @@
 
 #include "parser.h"
 
+void check(const char *test, parser_t *root) {
+  int status = parse(test, root);
+  printf("Testing: %s: %d\n", test, status);
+}
+
 int test_parse() {
   parser_t *root = init_tree();
 
   node_t *tree = (node_t *)malloc(sizeof(node_t));
-  tree->message = (string_t) {.data = (char *)"He", .size = strlen("He")};
-  tree->next = (basic_node_t) {.node = 0, .size = 0};
+  tree->message = (string_t){.data = (char *)"He", .size = strlen("He")};
+  tree->next = (basic_node_t){.node = 0, .size = 0};
   tree->value = 1;
 
-  root->next = (basic_node_t) {.node = tree, .size=1};
+  root->next = (basic_node_t){.node = tree, .size = 1};
 
   add_node("ab", 2, tree);
   add_node("cd", 3, tree);
   add_node("ef", 7, tree);
 
-  int status;
-
-  //status = parse("He", root);
-  //printf("Status He: %d\n", status == 1);
-
-  status = parse("Heab", root);
-  printf("Status Heab: %d\n", status);
-
-
-  status = parse("Hecd", root);
-  printf("Status Hecd: %d\n", status);
-
-  status = parse("Hellok", root);
-  printf("Status Hellok: %d\n", status);
+  check("He", root);
+  check("Hecd", root);
+  check("Heef", root);
 
   free_tree(root);
-  return status;
+  return 0;
 }
 
 int main() {
