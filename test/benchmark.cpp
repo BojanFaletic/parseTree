dfd5e93918d7332d249202de8a20b170d431b544
Bojan <bojanf.1996@gmail.com>
2022-11-21 03:02:25 +0100

    Devel (#4)
    * benchmark code

---
+++ test/benchmark.cpp
@@ -0,0 +1,22 @@
+#include <benchmark/benchmark.h>
+#include "parser.h"
+
+static void SingleParse(benchmark::State& state) {
+  // Perform setup here
+  // init parser object
+  parser_t *root;
+  parser_init(&root);
+
+  // set parsing values
+  parser_add("Hello", 1, root);
+  parser_add("Hey", 2, root);
+
+  for (auto _ : state) {
+    // This code gets timed
+    parser_parse("Hello", root);
+  }
+}
+// Register the function as a benchmark
+BENCHMARK(SingleParse);
+// Run the benchmark
+BENCHMARK_MAIN();
