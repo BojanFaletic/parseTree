bba486149a619aea942a6c49614d6d123ebf52c0
Bojan Faletic <bojanf.1996@gmail.com>
2022-12-02 13:20:12 +0100

    1

--- test/test.cpp
+++ test/test.cpp
@@ -3,32 +3,6 @@
 #include "loader.h"
 #include "parser.h"
 
-TEST(Parser, read_list) {
-  parser_t *list;
-  json_obj_t *json;
-
-  parser_init(&list);
-  parser_load_json("test_out.json", list, &json);
-
-  EXPECT_EQ(parser_parse("SKFOJSC", list), 1453);
-
-  parser_free_json(json);
-  parser_free(list);
-}
-
-TEST(Parser, nb_of_nodes) {
-  parser_t *list;
-  json_obj_t *json;
-
-  parser_init(&list);
-  parser_load_json("test_out.json", list, &json);
-
-  EXPECT_EQ(parser_parse("SKFOJSC", list), 1453);
-
-  parser_free_json(json);
-  parser_free(list);
-}
-
 TEST(Parser, add_new) {
   const char *test_filename = "test_cases/add_new.json";
   parser_t *list;
